4389b2cc601cc795c690718cdc54ff4ff6f8f446
Sophia Dobkowitz <dobkowit@uni-potsdam.de>
2018-06-15 11:29:51 +0200

    correct coordinates DTB download

--- SoilDataPrep/R/GetDTB.R
+++ SoilDataPrep/R/GetDTB.R
@@ -4,13 +4,14 @@ GetDTB<-
     
     #d5 - FLOAT32
     url<-paste0("https://webmap.ornl.gov/ogcbroker/wcs?originator=SDAT&&daac_usernum=45306&service=WCS&version=1.0.0&request=GetCoverage&coverage=1304_5&crs=EPSG:4326&bbox=", 
-                bbox(catch)[1,1], ",", bbox(catch)[1,2], ",", bbox(catch)[2,1], ",", bbox(catch)[2,2],
+                floor(bbox(catch)[1]), ",", floor(bbox(catch)[2]), ",", ceiling(bbox(catch)[3]), ",", ceiling(bbox(catch)[4]),
                 "&resx=0.008333333333333&resy=0.008333333333333&format=GeoTIFF_FLOAT32&interpolation=NEAREST")
+    
     download.file(url, destfile="Pelletier_DTB/depth_5.tif", mode="wb")
     
     #d6 - BYTE
     url<-paste0("https://webmap.ornl.gov/ogcbroker/wcs?originator=SDAT&&daac_usernum=45306&service=WCS&version=1.0.0&request=GetCoverage&coverage=1304_6&crs=EPSG:4326&bbox=",
-                bbox(catch)[1,1], ",", bbox(catch)[1,2], ",", bbox(catch)[2,1], ",", bbox(catch)[2,2],
+                floor(bbox(catch)[1]), ",", floor(bbox(catch)[2]), ",", ceiling(bbox(catch)[3]), ",", ceiling(bbox(catch)[4]),
                 "&resx=0.008333333333333&resy=0.008333333333333&format=GeoTIFF_BYTE&interpolation=NEAREST")
     download.file(url, destfile="Pelletier_DTB/depth_6.tif", mode="wb") 
   }  
